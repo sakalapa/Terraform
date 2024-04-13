f85baeb61115b7230ae4af6d562607bba534b92c
mak2016 <ajmal.consult@gmail.com>
2024-04-13 07:51:31 +0100

    updated main.tf with vlog info

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
+resource "aws_instance" "vlog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.blog.id]
@@ -28,7 +28,7 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_security_group" "blog" {
+resource "aws_security_group" "vlog" {
   name = "blog"
   tags = {
     Terraform = "true"
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "vlog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 }
 
 
-resource "aws_security_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "vlog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -56,7 +56,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 }
 
 
-resource "aws_security_group_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "vlog_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
