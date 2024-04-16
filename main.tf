c9d0267004295330d74354fb8f41905c94aaacd5
vasylivanchyk330 <60069876+vasylivanchyk330@users.noreply.github.com>
2024-04-16 21:04:52 +0200

    alb fix 9

--- main.tf
+++ main.tf
@@ -71,14 +71,21 @@ module "alb" {
     }
   ]
 
-
-  listeners =  {
-    http_tcp_listeners = {
+  http_tcp_listeners = [
+    {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
-    } 
-  }
+    }
+  ]
+
+#  listeners =  {
+#    http_tcp_listeners = {
+#      port               = 80
+#      protocol           = "HTTP"
+#      target_group_index = 0
+#    } 
+#  }
 
   tags = {
     Environment = "dev"
