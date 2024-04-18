fc440a881426769b0d7a5b15c34c1b7d88c6dc3e
vasylivanchyk330 <60069876+vasylivanchyk330@users.noreply.github.com>
2024-04-18 11:22:33 +0200

    added autoscale

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "instance_ami" {
-  value = aws_instance.blog.ami
-}
-
-output "instance_arn" {
-  value = aws_instance.blog.arn
-}
-
 output "public_dns" {
   value = aws_instance.blog.public_dns
 }
