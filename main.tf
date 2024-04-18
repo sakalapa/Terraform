0d4180f3a3619765c55712d37ace26e1eafe1667
vasylivanchyk330 <60069876+vasylivanchyk330@users.noreply.github.com>
2024-04-18 11:12:49 +0200

    alb fix 9 5 try

--- main.tf
+++ main.tf
@@ -46,52 +46,44 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+#####################
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    example_instance = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          port = 80
-          target_id    = aws_instance.blog.id
-        }
-      }
     }
-  }
+  ]
 
-  listeners = {
-    http_tcp_listener = {
+  http_tcp_listeners = [
+    {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  }
-
-
-#  listeners =  {
-#    http_tcp_listeners = {
-#      port               = 80
-#      protocol           = "HTTP"
-#      target_group_index = 0
-#    } 
-#  }
+  ]
 
   tags = {
     Environment = "dev"
   }
 }
+#######################
+
+
+
 
 module "blog_sg" {
   name                = "blog_new"
