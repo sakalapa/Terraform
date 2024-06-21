163cc9e8cf17a386be572e3efa5cdc3c0e7ed3bb
TommyS2001 <tommy.stopa01@gmail.com>
2024-06-21 15:24:54 +0100

    Load Balancer

--- main.tf
+++ main.tf
@@ -80,41 +80,6 @@ module "alb" {
   }
 
   }
-
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-    }
-  }
-
-  tags = {
-    Environment = "dev"
-    Project     = "Example"
-  }
 }
 
 module "blog_sg" {
