afd6326aa34aaf40e95dea1e5e15ee6981fb1a79
Gowri-Kumar <gowriskkumar@gmail.com>
2024-07-09 09:50:45 +1200

    updated dev vpc 1

--- main.tf
+++ main.tf
@@ -28,8 +28,6 @@ module "blog_vpc" {
   azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
   tags = {
     Terraform = "true"
     Environment = "dev"
