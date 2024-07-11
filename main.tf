f53aca60b5ff790e6d15897b593ea2ba04537f4e
ron46ruii4 <ron46ruii4@yahoo.com>
2024-07-11 19:16:07 +0300

    asd

--- main.tf
+++ main.tf
@@ -18,6 +18,14 @@ data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default" {
+  id = element(data.aws_subnet_ids.default.ids, 0)
+}
+
 resource "aws_vpc" "new_vpc" {
   cidr_block = "10.1.0.0/16"
   tags = {
@@ -35,11 +43,17 @@ resource "aws_subnet" "new_subnet" {
   }
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub") # Update this with the path to your public key
+}
+
 resource "aws_instance" "blog" {
   ami                   = data.aws_ami.app_ami.id
   instance_type         = var.instance_type
-  subnet_id             = data.aws_vpc.default.id
+  subnet_id             = data.aws_subnet.default.id
   vpc_security_group_ids = [aws_security_group.blog.id]
+  key_name              = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "HelloWorld"
@@ -51,6 +65,7 @@ resource "aws_instance" "new_blog1" {
   instance_type         = var.instance_type
   subnet_id             = aws_subnet.new_subnet.id
   vpc_security_group_ids = [aws_security_group.new_blog.id]
+  key_name              = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "new_blog1"
@@ -62,6 +77,7 @@ resource "aws_instance" "new_blog2" {
   instance_type         = var.instance_type
   subnet_id             = aws_subnet.new_subnet.id
   vpc_security_group_ids = [aws_security_group.new_blog.id]
+  key_name              = aws_key_pair.deployer.key_name
 
   tags = {
     Name = "new_blog2"
@@ -70,7 +86,7 @@ resource "aws_instance" "new_blog2" {
 
 resource "aws_security_group" "blog" {
   name        = "blog"
-  description = "Allow http, https, and ICMP in. Allow everything out"
+  description = "Allow http, https, SSH, and ICMP in. Allow everything out"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
@@ -83,6 +99,13 @@ resource "aws_security_group" "blog" {
   ingress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -104,7 +127,7 @@ resource "aws_security_group" "blog" {
 
 resource "aws_security_group" "new_blog" {
   name        = "new_blog"
-  description = "Allow http, https, and ICMP in. Allow everything out"
+  description = "Allow http, https, SSH, and ICMP in. Allow everything out"
   vpc_id      = aws_vpc.new_vpc.id
 
   ingress {
@@ -117,6 +140,13 @@ resource "aws_security_group" "new_blog" {
   ingress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
