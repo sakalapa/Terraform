c93fe0cd5bc2c070af005a5cd9121d40ecb63292
Ray Morgan <rayjaymor85@users.noreply.github.com>
2024-07-14 17:04:04 +1000

    Use module for SG

--- main.tf
+++ main.tf
@@ -30,6 +30,31 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+
+  name    = "blog_new"
+  vpc_id  = "data.aws_vpc.default.id"
+
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cider_blocks = ["0.0.0.0/0"]
+
+
+  egress_rules = ["all-all"]
+  egress_cider_blocks = ["0.0.0.0/0"]
+}
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
   vpc_id  = data.aws_vpc.default.id
